62dddcf6f43f601a7ab1084f8e78513455596b57
zenicc <ian.c.cook@live.co.uk>
2020-07-19 22:54:04 +0100

    ubuntu change

--- euler102.py
+++ euler102.py
@@ -96,6 +96,6 @@ if __name__ == '__main__':
 
     print("******* contained count =",containedCnt)
     print("******* not contained count =",notContainedCnt)
-    print("changed code")
+    print("changed code in ubuntu")
 
     print("euler102 took %f seconds" % (time.time() - st))
